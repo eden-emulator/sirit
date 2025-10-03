bd08bbda3d8f78f32aca14d6a9cd1948381c597e
crueter <crueter@eden-emu.dev>
2025-10-02 21:27:47 -0400

    fix openbsd platform sums
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/sums.sh
+++ .ci/sums.sh
@@ -1,11 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 # Generate SHA1, SHA256, SHA512 sums for all input files.
 # Requires: sha*sum, tr, cut
 
 for file in $@; do
     for algo in 1 256 512; do
-        if [ "$PLATFORM" = "openbsd" ]; then
+        if [ "$PLATFORM" = "openbsd-amd64" ]; then
             sha${algo} $file | awk '{print $4}' | tr -d "\n" > $file.sha${algo}sum
         else
             sha${algo}sum $file | cut -d " " -f1 | tr -d "\n" > $file.sha${algo}sum
