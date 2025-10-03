0d86a90c1c02061c79e6dac3600f669b48105219
crueter <crueter@eden-emu.dev>
2025-10-02 21:39:32 -0400

    fix changelog
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/changelog.sh
+++ .ci/changelog.sh
@@ -5,7 +5,8 @@
 
 # Change to the current repo
 BASE_DOWNLOAD_URL="https://github.com/eden-emulator/sirit/releases/download"
-TAG=v$VERSION
+TAG=$VERSION
+VERSION=$(echo $TAG | tr -d "v")
 
 artifact() {
   NAME="$1"
@@ -21,7 +22,11 @@ artifact() {
   echo
 }
 
-echo "Builds for sirit $VERSION"
+echo "This is version $VERSION of Eden's fork of sirit."
+echo
+echo "Compared to ReinUsesLisp's original project, this fork of sirit contains all of the changes made by RaphaelTheGreat (as seen in ShadPS4),"
+echo "alongside build system improvements and CI made by crueter and others. For use within projects, this should be identical to Raphael's fork,"
+echo "however UNIX-like operating systems can now install this as a system package and integrate it via CMake."
 echo
 echo "| Build | sha1sum | sha256sum | sha512sum |"
 echo "| ----- | ------- | --------- | --------- |"
@@ -29,10 +34,10 @@ echo "| ----- | ------- | --------- | --------- |"
 artifact Android android
 artifact "Windows (amd64)" windows-amd64
 artifact "Windows (arm64)" windows-arm64
-artifact Linux linux
+artifact "Linux (amd64)" linux-amd64
 artifact "Linux (aarch64)" linux-aarch64
-artifact Solaris solaris
-artifact FreeBSD freebsd
-artifact OpenBSD openbsd
+artifact "Solaris (amd64)" solaris-amd64
+artifact "FreeBSD (amd64)" freebsd-amd64
+artifact "OpenBSD (amd64)" openbsd-amd64
 artifact macOS macos-universal
 artifact Source source
